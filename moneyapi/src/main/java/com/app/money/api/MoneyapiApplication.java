340c5393e2f6ce0c1f1dcb45f5fe1af89bfdbd99
Edwin Lima <edwin.limaa@gmail.com>
2019-11-24 11:02:44 -0300

    Inicio do projeto, conexao MySql, pasta moneyweb.

---
+++ moneyapi/src/main/java/com/app/money/api/MoneyapiApplication.java
@@ -0,0 +1,13 @@
+package com.app.money.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyapiApplication.class, args);
+	}
+
+}
---
+++ moneyapi/src/test/java/com/app/money/api/MoneyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.money.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
