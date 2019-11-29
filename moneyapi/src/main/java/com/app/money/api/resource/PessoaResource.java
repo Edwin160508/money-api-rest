2b77f0bc11f4425329efc812db41d9e9c4f8d75b
Edwin Pereira Oliveira Lima <eplima1@stefanini.com>
2019-11-29 17:42:30 -0300

    Criando delete pessoaResource.

--- moneyapi/src/main/java/com/app/money/api/resource/PessoaResource.java
+++ moneyapi/src/main/java/com/app/money/api/resource/PessoaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,5 +69,11 @@ public class PessoaResource {
 		Optional<Pessoa> pessoaEncontrada = pessoaService.buscarCategoriaPorCodigo(codigo);
 		return ResponseEntity.ok().body(pessoaEncontrada.get());
 		
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Object> excluir(@PathVariable Long codigo){
+		pessoaService.excluir(codigo);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
