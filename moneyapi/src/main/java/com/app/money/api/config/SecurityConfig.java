8d7a1a29cf52fab4836d86217fbd7d94ac845396
Edwin Lima <edwin.limaa@gmail.com>
2019-12-09 10:40:02 -0300

    Usando Basic Atutentication.

---
+++ moneyapi/src/main/java/com/app/money/api/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.app.money.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	/*@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("{noop}admin").roles("ROLE");
+	
+	}*/
+	/**
+	 * Autenticação em memória 
+	 * Usando encode padrão BCryptPasswordEncoder()
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		auth.inMemoryAuthentication()
+		.withUser("admin").password(passwordEncoder().encode("admin")).roles("ROLE");
+	
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/api/categorias").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic()
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().csrf().disable();
+	}
+}
